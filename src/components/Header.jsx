cb2fcf7c3489c5fe6cf6c814597d8b8f908d642b
Your Name <ellycreativity8@gmail.com>
2026-02-16 17:59:41 +0200

    adding header on my project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
- export default function Header() {
+import globe from "../globe.png"
+
+export default function Header() {
     
 return (  
     <header>
-        <img src="" alt="" />
+        <img src={globe} alt="" />
+        <h1>My Travel journal.</h1>
     </header>
 )
  }
