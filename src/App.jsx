7fa0653e336159491ab09047e9d2650720ba6055
Your Name <ellycreativity8@gmail.com>
2026-02-16 18:26:06 +0200

    here is all about body but no props we have used I'm going to learn it right now

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Entry from "./components/Entry"
 
 function App() {
   
@@ -6,6 +7,9 @@ function App() {
     <>
 
   <Header/>
+   
+  <Entry/>
+
         </>
   )
 }
